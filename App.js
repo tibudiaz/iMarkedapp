00b15e383389dc51dcb8e0f7a87ea83da2b0c9e1
tibudiaz <augusto_diaz@outlook.com>
2023-04-19 20:49:16 -0300

    appcoder

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>coder.app</Text>
+      <Text>Hola. Coder</Text>
       <StatusBar style="auto" />
     </View>
   );
