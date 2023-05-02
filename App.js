20dc8295c017c8e1393d28146653fe06a268a7c3
tibudiaz <augusto_diaz@outlook.com>
2023-05-01 23:54:09 -0300

    actualizacion

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { View, KeyboardAvoidingView } from 'react-native';
+
+import {  View } from 'react-native';
 import { TopBar } from './components/TopBar';
 import { CatalogoList } from './components/catalogoList';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
+
+
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-        <View style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>   
+          <View style={{ flex: 1 }}>
           <TopBar />
           <CatalogoList />
-        </View>
-      </KeyboardAvoidingView>
+          </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
